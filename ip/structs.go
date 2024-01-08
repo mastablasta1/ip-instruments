b17129fce8c6d960db32f5b4cc69c2ea7383869d
mastablasta1 <93185543+mastablasta1@users.noreply.github.com>
2024-01-08 11:42:39 +0300

    Add files via upload

---
+++ ip/structs.go
@@ -0,0 +1,31 @@
+package ip
+
+type IpApiResponceStuct struct {
+	Status      string  `json:"status"`
+	Country     string  `json:"country"`
+	CountryCode string  `json:"countryCode"`
+	Region      string  `json:"region"`
+	RegionName  string  `json:"regionName"`
+	City        string  `json:"city"`
+	Zip         string  `json:"zip"`
+	Lat         float64 `json:"lat"`
+	Lon         float64 `json:"lon"`
+	Timezone    string  `json:"timezone"`
+	Isp         string  `json:"isp"`
+	Org         string  `json:"org"`
+	As          string  `json:"as"`
+	Query       string  `json:"query"`
+}
+
+type ResponceStruct struct {
+	ErrorInfo   bool    `json:"error_info"`
+	ErrorName   string  `json:"error_name"`
+	Status      string  `json:"status"`
+	Country     string  `json:"country"`
+	CountryCode string  `json:"countryCode"`
+	City        string  `json:"city"`
+	Lat         float64 `json:"lat"`
+	Lon         float64 `json:"lon"`
+	Timezone    string  `json:"timezone"`
+	Query       string  `json:"query"`
+}
